43e6b17fd46514a782e9d4c90f6b9c8ec7c0783e
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-04-25 14:05:32 +0800

    update viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
 
-  server: {
-    proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads/": "http://localhost:3000",
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api/": "http://localhost:3000",
+  //     "/uploads/": "http://localhost:3000",
+  //   },
+  // },
 
   build: {
     outDir: "build", // Specify the output directory here
